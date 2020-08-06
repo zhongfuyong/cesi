b92e9383133f2770a90ade3419aa4d7b3ae81b51
zhongfuyong <2362495326@qq.com>
2020-08-06 10:18:31 +0800

    8.6界面库

---
+++ xuexi/HZHControls/Form1.cs
@@ -0,0 +1,30 @@
+﻿using HZH_Controls.Controls;
+using HZH_Controls.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HZHControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click_1(object sender, EventArgs e)
+        {
+            FrmWithTitle fw = new FrmWithTitle();
+            fw.IsShowCloseBtn = true;
+            fw.Title = "xxx";
+            fw.Show();
+        }
+    }
+}
